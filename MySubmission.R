183b8861435c73ea10441e0a8f661c391db12459
TestStudent246 <bergen.silas@gmail.com>
2016-08-17 12:19:48 -0500

    Delete MySubmission.R

--- MySubmission.R
+++
@@ -1,8 +0,0 @@
-#For this assignment, create a scatterplot of speed versus distance using the "cars" data set in R.  Use blue squares as your points.
-#Some starter code:
-
-library(ggplot2)
-data(cars)
-ggplot(aes(x=dist,y=speed), data = cars) + geom_point(color='blue',point=14)
-#Nice work, but add the blue squares as data points!
-#good work!
